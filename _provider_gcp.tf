7dd3f6030171c9081e9a70c4792d0f2eec7f32a1
ZeePal <pallsjh@gmail.com>
2020-10-07 18:44:35 +1100

    upgraded to recommend provider versioning format

--- _provider_gcp.tf
+++
@@ -1,5 +0,0 @@
-provider google {
-  version = "~> 3.41"
-}
-
-data google_client_config config {}
---
+++ versions.tf
@@ -0,0 +1,6 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    google = { version = "~> 3.41" }
+  }
+}
